2848b543fb761a0532a4a5ae1d5b0dee91a38bd8
dare2do <dare2do@qq.com>
2018-09-21 15:54:39 +0800

    path fix

--- src/pages/page-ssh/index.vue
+++ src/pages/page-ssh/index.vue
@@ -5,8 +5,8 @@
   </d2container>
 </template>
 <script>
-const devlist = (resolve) => require.ensure([], () => resolve(require('@/components/devlist')), 'devlist')
-const webssh = (resolve) => require.ensure([], () => resolve(require('@/components/webssh')), 'webssh')
+const devlist = (resolve) => require.ensure([], () => resolve(require('./devlist')), 'devlist')
+const webssh = (resolve) => require.ensure([], () => resolve(require('./webssh')), 'webssh')
 
 export default {
   name: 'page-ssh',
